1ee94805e1923399bd8853012d1fb0b2a7795a60
wangyaozhou <wangyaozhou@youxin.com>
2018-11-30 18:01:28 +0800

    改名

--- index.php
+++ index.php
@@ -8,20 +8,20 @@
           href="/css/style.css"/>
     <!--<link rel="pingback" href="http://www.sandcomp.com/blog/xmlrpc.php"/>-->
 
-    <script type="text/javascript">//<![CDATA[
-    // Google Analytics for WordPress by Yoast v4.2.3 | http://yoast.com/wordpress/google-analytics/
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-19950260-1']);
-    _gaq.push(['_trackPageview']);
-    (function () {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-    //]]></script>
+//    <script type="text/javascript">//<![CDATA[
+//    // Google Analytics for WordPress by Yoast v4.2.3 | http://yoast.com/wordpress/google-analytics/
+//    var _gaq = _gaq || [];
+//    _gaq.push(['_setAccount', 'UA-19950260-1']);
+//    _gaq.push(['_trackPageview']);
+//    (function () {
+//        var ga = document.createElement('script');
+//        ga.type = 'text/javascript';
+//        ga.async = true;
+//        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//        var s = document.getElementsByTagName('script')[0];
+//        s.parentNode.insertBefore(ga, s);
+//    })();
+//    //]]></script>
 
     <script type='text/javascript'
             src='/js/comment-reply.js?ver=3.4.2'></script>
